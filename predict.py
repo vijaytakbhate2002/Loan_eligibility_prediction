58fb334a5442d3c1c372a2013b7df70200063b3a
Vijay Takbhate <vijaytakbhate20@gmail.com>
2024-07-07 22:56:55 +0530

    seven commit

--- predict.py
+++ predict.py
@@ -4,20 +4,30 @@ from prediction_model.config import config
 import numpy as np
 import pandas as pd
 import os
+import warnings
+warnings.filterwarnings('ignore')
 
-# classification_pipeline = dh.load_pipeline(config.MODEL)
+classification_pipeline = dh.load_pipeline(config.MODEL)
 
-def predict() -> None:
+# def predict() -> None:
+#     """Args: None
+#         Return: None
+        
+#         func: takes user data, process it, do prediction"""
+#     user_data = pd.read_csv(os.path.join(config.DATASETS_PATH, config.TEST_DATA))
+#     user_data = user_data[config.FEATURES]
+#     pred = classification_pipeline.predict(X=user_data)
+#     output = np.where(pred == 0,'N','Y')
+#     return output
+
+def predict(user_data) -> None:
     """Args: None
         Return: None
         
         func: takes user data, process it, do prediction"""
-    user_data = pd.read_csv(os.path.join(config.DATASETS_PATH, config.TEST_DATA))
-    user_data = user_data[config.FEATURES]
     pred = classification_pipeline.predict(X=user_data)
-    output = np.where(pred == 0,'N','Y')
+    output = np.where(pred == 0, 'N','Y')
     return output
 
 if __name__ == "__main__":
     res = predict()
- 
--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -15,7 +15,7 @@ import pytest
 
 
 @pytest.fixture
-def test_prediction():
+def single_prediction():
     """ Args: None
         Return: np.ndarray (predicted output) """
     test_data = pd.read_csv(os.path.join(config.DATASETS_PATH, config.TEST_DATA))
@@ -23,12 +23,12 @@ def test_prediction():
     res = predict(first_row)
     return res
 
-def test_none_out():
-    assert test_prediction.get('predictions')[0] is not None
+def test_none_out(single_prediction):
+    assert single_prediction is not None
 
-def test_str_out():
-    assert isinstance(test_prediction.get('predictions')[0], str)
+def test_str_out(single_prediction):
+    assert type(single_prediction[0]) == np.str_
 
-def test_y_out():
-    assert test_prediction.get('predictions')[0] == 'Y'
+def test_y_out(single_prediction):
+    assert single_prediction[0] == 'Y'
 
