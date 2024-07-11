99c001eb2190bbdd87b7ddf272397866091c825f
Vijay Takbhate <vijaytakbhate20@gmail.com>
2024-07-11 22:20:52 +0530

    ten commit

--- setup.py
+++ setup.py
@@ -1,19 +1,65 @@
+import io # provides compatibility between python 2 and python 3
 import os
-import io
 from pathlib import Path
-from prediction_model.config import config
-import sys
 
-NAME = "Loan Eligibility Prediction"
-DESCRIPTION = "Machine Learning model for loan eligibility prediction"
-URL = 'https://github.com/vijaytakbhate2002/Loan_eligibility_prediction.git'
-EMAIL = 'takbhatevijay@gmail.com'
-AUTHOR_NAME = 'Vijay Dipak Takbhate'
-REQUIRED_PYTHON = '>=3.12.4'
+from setuptools import find_packages, setup
 
-# reading requirements.txt file
-def read_req(fname='reqirements.txt', encoding='utf8') -> None:
-    with io.open(os.path.join(config.ROOT_PATH, fname)) as f:
-        return f.read().splitlines()
-    
-# 
+
+# Metadata of package
+NAME = 'prediction_model'
+DESCRIPTION = 'Loan Prediction Model'
+URL = 'https://github.com/vijaytakbhate2002'
+EMAIL = 'vijaytakbhate20@gmail.com'
+AUTHOR = 'Vijay Dipak Takbhate'
+REQUIRES_PYTHON = '>=3.12.4'
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+
+# Get the list of packages to be installed
+def list_reqs(fname='requirements.txt'):
+    try:
+        with io.open(os.path.join(pwd, fname), encoding='utf-8') as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        pass
+
+try:
+    with io.open(os.path.join(pwd, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+# Load the package's __version__.py module as a dictionary.
+ROOT_DIR = Path(__file__).resolve().parent
+PACKAGE_DIR = ROOT_DIR / NAME
+about = {}
+with open(PACKAGE_DIR / 'VERSION') as f:
+    _version = f.read().strip()
+    about['__version__'] = _version
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    package_data={'prediction_model': ['VERSION']},
+    install_requires=list_reqs(),
+    extras_require={},
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
