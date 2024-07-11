9207564962b1a99a06759e6e133adec09f76253b
Vijay Takbhate <vijaytakbhate20@gmail.com>
2024-07-11 21:34:58 +0530

    nine commit

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-import sys
-from prediction_model.processing import preprocessing
+import os
+import io
+from pathlib import Path
 from prediction_model.config import config
+import sys
 
-print(len(sys.path))
-for path in sys.path:
-    print(path.split('\\')[-1])
+NAME = "Loan Eligibility Prediction"
+DESCRIPTION = "Machine Learning model for loan eligibility prediction"
+URL = 'https://github.com/vijaytakbhate2002/Loan_eligibility_prediction.git'
+EMAIL = 'takbhatevijay@gmail.com'
+AUTHOR_NAME = 'Vijay Dipak Takbhate'
+REQUIRED_PYTHON = '>=3.12.4'
+
+# reading requirements.txt file
+def read_req(fname='reqirements.txt', encoding='utf8') -> None:
+    with io.open(os.path.join(config.ROOT_PATH, fname)) as f:
+        return f.read().splitlines()
+    
+# 
