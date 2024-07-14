5f35933714037922c42b1581136cb6b119bacc5f
Vijay Takbhate <vijaytakbhate20@gmail.com>
2024-07-14 22:03:09 +0530

    v1

---
+++ build/lib/prediction_model/processing/data_handling.py
@@ -0,0 +1,27 @@
+from ..config import config
+import pandas as pd
+import os
+import joblib
+from sklearn.base import ClassifierMixin
+
+def load_dataset(file_name:str) -> pd.DataFrame:
+    """Args : file_name (str)
+        Return : pd.DataFrame """
+    path = os.path.join(config.DATASETS_PATH, file_name)
+    df = pd.read_csv(path)
+    return df
+
+# Serialization
+def dump_pipeline(pipeline_to_save) -> None:
+    """Args : pipeline_to_save
+        Return : None"""
+    joblib.dump(pipeline_to_save, os.path.join(config.TRAINED_MODELS_PATH, config.MODEL))
+    print("Pipeline dumped successfully")
+
+# Deserialization
+def load_pipeline(pipeline_to_load) -> ClassifierMixin:
+    """Args : pipeline_to_load
+        Return : ClassifierMixin (Model)"""
+    classifier = joblib.load(os.path.join(config.TRAINED_MODELS_PATH, config.MODEL))
+    print("Pipeline loaded successfully")
+    return classifier
---
+++ build/lib/prediction_model/processing/preprocessing.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import numpy as np
+from typing_extensions import Union
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import LabelEncoder
+    
+class MeanImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, cols:list) -> None:
+        """Args: cols (columns for mean imputation)
+            Return: None"""
+        self.cols = cols
+
+    def fit(self, X:pd.DataFrame, y:Union[pd.DataFrame, pd.Series]=None):
+        """Args: X (df), y (target column/s)
+            Return: self (Nothing)"""
+        self.mean_dict = {}
+        for col in self.cols:
+            self.mean_dict[col] = X[col].mean()
+        return self
+    
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        X = X.copy()
+        for col in self.cols:
+            X[col] = X[col].fillna(self.mean_dict[col])
+        return X
+    
+class ModeImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, cols:list) -> None:
+        """Args: cols (columns for mode imputation)
+            Return: None"""
+        self.cols = cols
+
+    def fit(self, X:pd.DataFrame, y:Union[pd.DataFrame, pd.Series]=None):
+        """Args: X (df), y (target column/s)
+            Return: self (Nothing)"""
+        self.mode_dict = {}
+        for col in self.cols:
+            self.mode_dict[col] = X[col].mode()[0]
+        return self
+    
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        X = X.copy()
+        for col in self.cols:
+            X[col] = X[col].fillna(self.mode_dict[col])
+        return X
+    
+class LabelEncode(BaseEstimator, TransformerMixin):
+    def __init__(self, cols:list) -> None:
+        """Args: cols (list)
+            Return: None"""
+        self.cols = cols
+        
+    def fit(self, X:pd.DataFrame, y=None):
+        """Args: X (Pandas DataFrame)
+            Return: self"""
+        self.encoder = LabelEncoder()
+        return self
+
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """Args: Nothing to show
+            Return: X (Label Encoded pandas dataframe)"""
+        X = X.copy()
+        for col in self.cols:
+            X[col] = self.encoder.fit_transform(X[col])
+        return X
+
+class LogTransform(BaseEstimator, TransformerMixin):
+    def __init__(self, cols:list) -> None:
+        self.cols = cols
+
+    def fit(self, X, y=None):
+        """Args: X (df), y (target column/s)
+            Returns: self
+            
+            func: Replacing Zeros with 0.0001
+            """
+        self.X = X.copy()
+        for col in self.cols:
+            self.X[col] = self.X[col].apply(lambda x: 0.0001 if x == 0 else x)
+        return self
+    
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """Args: Nothing to show
+            Return: pd.DataFrame
+            func: Log transfomrmation of self.X"""
+        for col in self.cols:
+            self.X[col] = np.log(self.X[col])
+        return self.X
+
+class DropColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, cols:list) -> None:
+        """Args: cols (list)
+            Return: None"""
+        self.cols = cols
+        
+    def fit(self, X, y=None):
+        """Args: X (Pandas DataFrame)
+            Return: self"""
+        return self
+
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        """Args: Nothing to show
+            Return: X (with dropped columns)"""
+        X = X.copy()
+        X = X.drop(self.cols, axis='columns')
+        return X
+
+
