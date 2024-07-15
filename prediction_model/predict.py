13c19f9fec9a3bd4a922d912b4fcc51fc682ee5b
Vijay Takbhate <vijaytakbhate20@gmail.com>
2024-07-15 20:27:27 +0530

    final commit

--- prediction_model/predict.py
+++ prediction_model/predict.py
@@ -9,8 +9,8 @@ warnings.filterwarnings('ignore')
 
 classification_pipeline = dh.load_pipeline(config.MODEL)
 
-def predict(user_data:np.ndarray) -> str:
-    """Args: np.ndarray
+def predict(user_data:pd.DataFrame) -> str:
+    """Args: pd.DataFrame
         Return: str
         
         func: takes user data, process it, do prediction"""
@@ -18,7 +18,6 @@ def predict(user_data:np.ndarray) -> str:
         raise ValueError("Unsupported data type, expected pd.DataFrame")
     if isinstance(user_data, (pd.DataFrame)):
         user_data = user_data[config.FEATURES]
-
     pred = classification_pipeline.predict(user_data)
     output = 'N' if pred[0] == 0 else 'Y'
     return output
